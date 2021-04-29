ec26e2fda198e33c5b50272e431ac64e504c32f6
MonsignorEduardo <monsignor.eduardo@gmail.com>
2021-04-29 18:55:20 +0200

    Fixed port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ app.get('/', (_req, res) => {
   res.send('Hello world!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
