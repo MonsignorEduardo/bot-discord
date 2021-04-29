fb08d1e5b10d19b4a6b58e5214f9a7d8652dceb3
MonsignorEduardo <monsignor.eduardo@gmail.com>
2021-04-29 19:19:05 +0200

    a ver si ahora va

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
-import * as express from 'express';
 import starDiscord from './discord';
-
-const app = express();
-app.get('/', (_req, res) => {
-  res.send('Hello world!');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
 
 starDiscord();
