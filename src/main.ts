979b301de79a1a8320e62fb797f22f485d9f780b
MonsignorEduardo <monsignor.eduardo@gmail.com>
2021-04-30 13:48:27 +0200

    Juan juan
    Co-authored-by: Estumon <Estumon@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import starDiscord from './discord';
-
+import startTelegram from './telegram';
 if (process.env.NODE_ENV === 'LOCAL') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('dotenv').config();
 }
 
 starDiscord();
+startTelegram(process.env.TELEGRAM as string);
--- src/telegram.ts
+++ src/telegram.ts
@@ -1,12 +1,16 @@
 import {Telegraf} from 'telegraf';
 
-const bot = new Telegraf(process.env.BOTTOKEN);
-bot.start(ctx => ctx.reply('Welcome'));
-bot.help(ctx => ctx.reply('Send me a sticker'));
-bot.on('sticker', ctx => ctx.reply('👍'));
-bot.hears('hi', ctx => ctx.reply('Hey there'));
-bot.launch();
+export default function startTelegram(tokenTelegram: string) {
+  const bot = new Telegraf(tokenTelegram);
+  bot.start(ctx => ctx.reply('Welcome'));
+  bot.help(ctx => ctx.reply('Send me a sticker'));
+  bot.hears('carlos', ctx => ctx.reply('sssss'));
+  bot.on('sticker', ctx => ctx.reply('👍'));
+  bot.hears('hi', ctx => ctx.reply('Hey there'));
+  bot.hears('cris', ctx => ctx.reply('No'));
+  bot.launch();
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  // Enable graceful stop
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
