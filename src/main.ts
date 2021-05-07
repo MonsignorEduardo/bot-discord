4d969ea30827d8c41813dbeaaaa761b829daf2d8
MonsignorEduardo <monsignor.eduardo@gmail.com>
2021-05-07 12:46:05 +0200

    🔨 Usamos env siempre

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import starDiscord from './discord';
 import startTelegram from './telegram';
-if (process.env.NODE_ENV === 'LOCAL') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('dotenv').config();
-}
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 starDiscord();
-startTelegram(process.env.TELEGRAM as string);
+//startTelegram(process.env.TELEGRAM as string);
